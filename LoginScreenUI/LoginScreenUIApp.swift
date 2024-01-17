3a60fa2c2beefe5dfff92e516e371b68bbd886b8
Sanil Sarathe <sanilsarathe76@gmail.com>
2024-01-17 08:23:54 +0530

    Login Screen UI in Swift UI

---
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by macmini45 on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
